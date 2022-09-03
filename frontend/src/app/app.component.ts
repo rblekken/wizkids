14579182bfdc9741a7bdb4b49bfc9c842ece8051
Runar Blekken <runar.blekken@gmail.com>
2022-09-04 00:38:02 +0200

    better ux - angular material and flexbox

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,17 +11,17 @@ export class AppComponent {
   dictionaryWords:string[] = [];
 
   async textChanged(event: KeyboardEvent){
-    let target = event.target as HTMLInputElement;
-    const response = await fetch(`/api/Prediction?word=${target.value}&language=${this.language}`);
+    let text = (event.target as HTMLInputElement).value;
+    if(text.length == 0)
+      return;
+    const response = await fetch(`/api/Prediction?word=${text}&language=${this.language}`);
     const predictionResult = await response.json();
     this.customWords = predictionResult.customWords;
     this.dictionaryWords = predictionResult.dictionaryWords;
   }
 
- switchLanguage(){
-    if(this.language === "en-GB")
-      this.language = "da-DK";
-    else
-      this.language = "en-GB";
+  onLanguageChanged(lang:string){
+    this.language = lang;
   }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
